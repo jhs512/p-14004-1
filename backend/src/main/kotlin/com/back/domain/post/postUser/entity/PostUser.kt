6a8c17b7f556e18391fefc6354f3a79a88b64b02
jhs512 <jangka512@gmail.com>
2025-08-28 09:35:55 +0900

    work

--- backend/src/main/kotlin/com/back/domain/post/postUser/entity/PostUser.kt
+++ backend/src/main/kotlin/com/back/domain/post/postUser/entity/PostUser.kt
@@ -1,21 +1,20 @@
 package com.back.domain.post.postUser.entity
 
+import com.back.domain.member.member.entity.BaseMember
 import com.back.domain.member.member.entity.Member
-import com.back.global.jpa.entity.BaseTime
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
-import org.hibernate.annotations.NaturalId
 
 @Entity
 @Table(name = "MEMBER")
 class PostUser(
     id: Int,
-    @NaturalId @field:Column(unique = true) val username: String,
+    username: String,
     @Column(name = "nickname") var name: String,
     @Column(columnDefinition = "INT DEFAULT 0") var postsCount: Int = 0,
     @Column(columnDefinition = "INT DEFAULT 0") var postCommentsCount: Int = 0,
-) : BaseTime(id) {
+) : BaseMember(id, username) {
     constructor(member: Member) : this(
         member.id,
         member.username,
