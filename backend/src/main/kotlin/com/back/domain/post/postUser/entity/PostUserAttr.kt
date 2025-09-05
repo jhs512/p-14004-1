e00a5c1a9c19472237f529d4a211073056e53b6d
jhs512 <jangka512@gmail.com>
2025-09-05 14:15:05 +0900

    work

--- backend/src/main/kotlin/com/back/domain/post/postUser/entity/PostUserAttr.kt
+++ backend/src/main/kotlin/com/back/domain/post/postUser/entity/PostUserAttr.kt
@@ -1,13 +1,18 @@
 package com.back.domain.post.postUser.entity
 
 import com.back.global.jpa.entity.BaseTime
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.ManyToOne
+import jakarta.persistence.*
 
 @Entity
+@Table(
+    uniqueConstraints = [
+        UniqueConstraint(columnNames = ["subject_id", "name"])
+    ]
+)
 class PostUserAttr(
-    @field:ManyToOne val subject: PostUser,
+    @field:ManyToOne
+    @field:JoinColumn(name = "subject_id")
+    val subject: PostUser,
     val name: String,
     @field:Column(name = "val", columnDefinition = "TEXT") var value: String,
 ) : BaseTime() {
